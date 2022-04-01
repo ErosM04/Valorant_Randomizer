2221cd6258a9fa11016bd7e4670939faf8076acb
ErosM04 <eros.menin@gmail.com>
2022-04-01 09:16:37 +0200

    found a way to set the background

--- Behavior.js
+++ Behavior.js
@@ -1,12 +1,13 @@
 var maps = ["SPLIT","HAVEN","ASCENT","ICEBOX","BIND"]; //Fracture non la vuole nessuno
 var agents = ["JETT","SAGE","SOVA","REYNA","OMEN","BREACH","BRIMSTONE","PHOENIX","RAZE","KILLJOY","CYPHER","VIPER","SKYE","YORU","ASTRA","KAY/O","CHAMBER","NEON"];
-var background1 = ".\\images\\Background1.jpg";
+var background1 = "Background1";
+var background2 = "Background2"; //To define
 
 
 function start(){
-    display_background(background1);
+    set_background(background1);
 }
 
-function display_background(img){
-    
+function set_background(img){
+    document.body.style.backgroundImage = "url('\\images\\" + img + ".png')";
 }
