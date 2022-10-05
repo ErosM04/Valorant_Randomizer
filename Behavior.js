fec124675835f32e6bfadacbc189d00b852099c6
Menin Eros <eros.menin@cz.lan>
2022-10-05 09:10:43 +0200

    Added some empty methods

--- Behavior.js
+++ Behavior.js
@@ -1,13 +1,21 @@
-var maps = ["SPLIT","HAVEN","ASCENT","ICEBOX","BIND"]; //Fracture non la vuole nessuno
+var maps = ["SPLIT","HAVEN","ASCENT","ICEBOX","BIND", "PEARL"]; //Fracture non la vuole nessuno
 var agents = ["JETT","SAGE","SOVA","REYNA","OMEN","BREACH","BRIMSTONE","PHOENIX","RAZE","KILLJOY","CYPHER","VIPER","SKYE","YORU","ASTRA","KAY/O","CHAMBER","NEON"];
 var background1 = "Background1";
 var background2 = "Background2"; //To define
 
 
 function start(){
-    set_background(background1);
+    // set_background(background1);
 }
 
 function set_background(img){
     document.body.style.backgroundImage = "url('./images/" + img + ".jpg')";
+}
+
+function getSquadsArray(players){
+    console.log(typeof(players));
+}
+
+function getRandomMap(){
+    return maps[Math.floor(Math.random() * maps.length) + 1];
 }
