4c087269aab851b0ee64e15a9d12bc8d7eab619d
Eros Menin <eros.menin@gmail.com>
2023-07-07 17:59:55 +0200

    completed agents and maps generation

--- menus.js
+++ menus.js
@@ -1,11 +1,73 @@
-function buildMenu(){
-    getMapsData().then((res) => console.log(res));
+const unwantedMaps = ['District', 'Kasbah', 'Piazza', 'The Range'];
+
+async function buildMenus(){
+    let agents = await getAgentsMap();
+    let maps = await getMapsMap();
+    
+    for (const [key, value] of agents.entries()) {
+        buildAgentCell(key, value);
+    }
+
+    for (const [key, value] of maps.entries()) {
+        if(!unwantedMaps.includes(key))
+            buildMapCell(key, value);
+        
+    }
+}
+
+function buildAgentCell(name, image){
+    // Creates the div
+    let div = document.createElement('div');
+    div.className = "deselect-div";
+    // Creates the image
+    let img = document.createElement('img');
+    img.src = image;
+    img.className = 'agent-deselect-img';
+    // Creates the checkbox
+    let check = document.createElement('input');
+    check.type = 'checkbox';
+    check.className = 'deselect-checkbox';
+    check.id = 'check-agent-' + name;
+    check.checked = true;
+
+    //Appends the elements
+    div.appendChild(img);
+    div.appendChild(check);
+    document.getElementById('agent-selector-div').appendChild(div);
+}
+
+function buildMapCell(name, image){
+    // Creates the div
+    let div = document.createElement('div');
+    div.className = "deselect-div";
+    // Creates the image
+    let img = document.createElement('img');
+    img.src = image;
+    // img.width = '150';
+    // img.style.marginInline = '50';
+    img.className = 'map-deselect-img';
+    // Creates the text
+    let text = document.createElement('p');
+    text.className = 'map-text';
+    text.innerText = name;
+    // Creates the checkbox
+    let check = document.createElement('input');
+    check.type = 'checkbox';
+    check.className = 'deselect-checkbox';
+    check.id = 'check-map-' + name;
+    check.checked = true;
+
+    //Appends the elements
+    div.appendChild(img);
+    div.appendChild(text);
+    div.appendChild(check);
+    document.getElementById('map-selector-div').appendChild(div);
 }
 
 /**
  * Map['agent-name'] => icon-link
  */
-async function getAgentsData(){
+async function getAgentsMap(){
     const res = await fetch("https://valorant-api.com/v1/agents?isPlayableCharacter=true");
     const agents = await res.json();
     const agentsMap = new Map();
@@ -16,7 +78,7 @@ async function getAgentsData(){
 /**
  * Map['map-name'] => icon-link
  */
-async function getMapsData(){
+async function getMapsMap(){
     const res = await fetch("https://valorant-api.com/v1/maps");
     const maps = await res.json();
     const mapMap = new Map();
